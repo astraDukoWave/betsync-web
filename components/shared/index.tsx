46f575be64dbccbb8587af3b02608fc1f28ab24d
astraDukoWave <astradukowave@gmail.com>
2026-03-11 13:12:28 -0600

    feat(sprint2): add components/shared/index.tsx barrel export

---
+++ components/shared/index.tsx
@@ -0,0 +1,7 @@
+// Barrel export for shared components
+export { StatusBadge } from "./StatusBadge";
+export { GradeBadge } from "./GradeBadge";
+export { CLVBadge } from "./CLVBadge";
+export { OddsChip } from "./OddsChip";
+export { SkeletonCard } from "./SkeletonCard";
+export { ErrorState } from "./ErrorState";
